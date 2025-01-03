2966b822f30df91e638df18dea81e9b2b6a24d70
Ruth Campion <97449315+RJCart@users.noreply.github.com>
2025-01-03 22:46:09 +0000

    Added hypothesis

---
+++ Code/Code.R
@@ -0,0 +1,11 @@
+#Hypothesis: For males and females between the ages of 18 and 40, VR based therapy is more effective than traditional therapy in alleviating symptoms of depression.
+
+
+
+library(tidyverse)
+
+summary(data)
+
+#Check if data needs to be cleaned
+colSums(is.na(data))
+
