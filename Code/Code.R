6cda5721c88699dee46be30099dee17a565fe1ec
Ruth Campion <97449315+RJCart@users.noreply.github.com>
2025-01-03 23:33:26 +0000

    add histogram to visualise pre treatment data

--- Code/Code.R
+++ Code/Code.R
@@ -1,11 +1,39 @@
 #Hypothesis: For males and females between the ages of 18 and 40, VR based therapy is more effective than traditional therapy in alleviating symptoms of depression.
 
+install.packages("ggplot2")
+class(data)
+
+depression_data <- read.csv("C:/Users/Ruth/Desktop/Ca1_RuthC/RC_Data_CA1/Data/participants_data_final.csv")
 
 
-library(tidyverse)
+# Test Plot
+ggplot(mtcars, aes(x = wt, y = mpg)) +
+  geom_point()
 
-summary(data)
 
-#Check if data needs to be cleaned
-colSums(is.na(data))
+# Load Packages
+library(ggplot2)
+
+
+
+head(depression_data)          # View first few rows
+dim(depression_data)           # Check dimensions (rows and columns)
+names(depression_data)         # Check column names
+
+#Pre Treatment Histogram
+ggplot(depression_data, aes(x = Pre_Treatment_Score)) +
+  geom_histogram(binwidth = 5, fill = "blue", color = "white") +
+  labs(title = "Distribution of Pre-Treatment Scores", x = "Pre-Treatment Score", y = "Frequency") +
+  theme_minimal()
+
+
+# Basic boxplot
+ggplot(depression_data, aes(x = Group, y = Change_in_Score, fill = Group)) +
+  geom_boxplot() +
+  labs(
+    title = "Change in Score by Group",
+    x = "Group",
+    y = "Change in Score"
+  ) +
+  theme_minimal()
 
